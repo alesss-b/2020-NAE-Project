6db7583c69428502b92e90fe54909d145f61d8ad
alesss-b <alexbradshaw21977@gmail.com>
2020-03-02 12:57:51 -0600

    Added Main and Formatted Data

---
+++ NAE_main.m
@@ -0,0 +1,58 @@
+%% 2020 NAE Project - Securing Cyber Space
+%   Alfredo Rogel       Alex Bradshaw
+%   Johan Gonzalez      Fernando Aguado 
+%   Chibuzor Prince     Zhenyu yu
+%   Albert ky
+
+%{ 
+Problem Statement:
+    Using a file containing information about data breaches, output 
+various visual representations of data corresponding to user input.
+Then, perform projection calculations to predict future breaches.
+%}   
+
+%{
+'breach_info.csv' layout:
+Column 1 - Abreveated States
+Column 2 - Entity Type
+Column 3 - Number of Individuals Affected
+Column 4 - Date
+Column 5 - Device/Location of Breach
+%}
+
+% Housekeeping:
+clc
+clear
+close all
+
+%% Load Data
+
+breach_data = readtable('breach_info.csv', 'Headerlines', 1);
+
+% Menu Prompts and Options:
+%   main:
+    mainPrompt = "What would you like to see?";
+    mainChoices = ["Line Graph", "Pie Chart", "Map Graph", "Projections", "Exit"];
+
+%% Main Loop
+mainChoice = menu(mainPrompt, mainChoices);
+while mainChoice ~= 5
+    % Main Menu Loop
+    switch mainChoice
+        case 1 
+            % Go To Line Graph
+            disp("Chosen Line Graph");
+        case 2
+            % Go To Pie Chart
+            disp("Chosen Pie Chart");
+        case 3
+            % Go To Map Graph
+            disp("Chosen Map Graph");
+        case 4
+            % GO To Projections
+            disp("Chosen Projections");
+    end
+mainChoice = menu(mainPrompt, mainChoices);
+end
+fprintf("Exiting Program...\n");
+
